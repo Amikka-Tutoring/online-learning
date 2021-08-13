4f8b28471e8a89870b41ac9e54321e7f7d69bd47
Edin Vllaço <edin.vllaco@gmail.com>
2021-08-13 11:10:30 +0200

    tailwind to bootstrap

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,43 @@
+<template>
+    <Head :title="title" />
+    <h1>Test</h1>
+
+</template>
+
+
+<script>
+
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+export default {
+    props: {
+        title: String,
+    },
+
+    components: {
+        Head,
+        Link,
+    },
+
+    data() {
+        return {
+            showingNavigationDropdown: false,
+        }
+    },
+
+    methods: {
+        switchToTeam(team) {
+            this.$inertia.put(route('current-team.update'), {
+                'team_id': team.id
+            }, {
+                preserveState: false
+            })
+        },
+
+        logout() {
+            this.$inertia.post(route('logout'));
+        },
+    }
+}
+</script>
+
---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-span-6 sm:col-span-4">
+        <jet-label for="current_password" value="Current Password" />
+        <jet-input id="current_password" type="password" class="mt-1 block w-full"  ref="current_password" autocomplete="current-password" />
+        <jet-input-error class="mt-2" />
+    </div>
+
+</template>
+<script>
+import Input from "@/Jetstream/Input";
+import JetFormSection from "@/Jetstream/FormSection";
+import JetInput from "@/Jetstream/Input";
+import JetInputError from "@/Jetstream/InputError";
+import JetLabel from "@/Jetstream/Label";
+export default {
+    components: {Input,
+        JetFormSection,
+        JetInput,
+        JetInputError,
+        JetLabel,}
+}
+</script>
